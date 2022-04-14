b6542cb3474f996bc4dcc61669abc74ac07d0fdf
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-14 14:51:55 -0700

    dynamic breed name

---
+++ breedFetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+
+const breedName = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+  console.log(JSON.parse(body));
+});
+
+
+
+
+// console.log(body);
+// const data = JSON.parse(body);
+
