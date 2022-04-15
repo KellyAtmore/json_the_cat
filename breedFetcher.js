2c9442a565ae67314678a1f2d35a74eb4719ff26
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-14 17:43:47 -0700

    changes to functions and tests made

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (!data || !data.length) {
+      callback("Breed Not Found", null);
+      return;
+    }
 
-const breedName = process.argv.slice(2);
+    callback(null, data[0].description);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-  console.log(JSON.parse(body));
-});
-
-
-
-
-// console.log(body);
-// const data = JSON.parse(body);
-
+module.exports = {fetchBreedDescription};
